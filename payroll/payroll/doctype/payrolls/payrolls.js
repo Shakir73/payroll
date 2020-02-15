3548c96be4520f7d7e6f241379fa06ed1b5873d2
Shakir73 <engr.abdul.bari.ansari@gmail.com>
2020-02-15 12:39:32 +0500

    p

--- payroll/payroll/doctype/payrolls/payrolls.js
+++ payroll/payroll/doctype/payrolls/payrolls.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2020, Abacus First Solutions and contributors
 // For license information, please see license.txt
 
+
 frappe.ui.form.on('Payrolls', {
 	no_of_pay_checks: function(frm) {
 		frm.set_value('total_no_of_pay_checks', frm.doc.no_of_direct_deposit + frm.doc.no_of_pay_checks);
@@ -11,6 +12,10 @@ frappe.ui.form.on('Payrolls', {
 			frappe.msgprint(__("You can not select past date"));
 			frappe.validated = false;
 		}
+	},
+
+	direct_deposit: function(frm) {
+		frm.set_df_property('no_of_direct_deposit', 'read_only', (frm.doc.direct_deposit) ? 0 : 1); // see you next time
 	},
 
 	pay_start_date: function(frm) {
@@ -36,7 +41,8 @@ frappe.ui.form.on('Payrolls', {
 				frm.set_value('pay_end_date', frappe.datetime.add_months(frm.doc.pay_start_date , 6));
 			}else if (frm.doc.payroll_schedule === 'Yearly') {
 				frm.set_value('pay_end_date', frappe.datetime.add_months(frm.doc.pay_start_date , 12));
-			}else frm.set_value('pay_end_date', '')
+			}
+			// else frm.set_value('pay_end_date', '')
 		// }
 	},
 
@@ -64,7 +70,98 @@ frappe.ui.form.on('Payrolls', {
 				frm.set_value('pay_start_date', frappe.datetime.add_months(frm.doc.pay_end_date , -6));
 			}else if (frm.doc.payroll_schedule === 'Yearly') {
 				frm.set_value('pay_start_date', frappe.datetime.add_months(frm.doc.pay_end_date , -12));
-			}else frm.set_value('pay_start_date', '')
+			}
+			// else frm.set_value('pay_start_date', '')
 		// }
+	},
+	time_unit: function(frm) {
+		if (frm.doc.time_unit === 'After 1 day') {
+			frm.set_value('check_date', frappe.datetime.add_days(get_today(), 1));
+		}
+		else if (frm.doc.time_unit === 'After 2 days') {
+			frm.set_value('check_date', frappe.datetime.add_days(get_today(), 2));
+		}
+		else if (frm.doc.time_unit === 'After 3 days') {
+			frm.set_value('check_date', frappe.datetime.add_days(get_today(), 3));
+		}
+		else if (frm.doc.time_unit === 'After 4 days') {
+			frm.set_value('check_date', frappe.datetime.add_days(get_today(), 4));
+		}else if (frm.doc.time_unit === 'After 5 days') {
+			frm.set_value('check_date', frappe.datetime.add_days(get_today(), 5));
+		}else if (frm.doc.time_unit === 'After 6 days') {
+			frm.set_value('check_date', frappe.datetime.add_days(get_today(), 6));
+		}else if (frm.doc.time_unit === 'After 7 days') {
+			frm.set_value('check_date', frappe.datetime.add_days(get_today(), 7));
+		}else if (frm.doc.time_unit === 'Next Sunday') {
+			let dayOfWeek = 0;
+			let date = new Date(get_today());
+			date.setDate(date.getDate() + (7 + dayOfWeek - date.getDay() - 1) % 7 +1);
+			frm.set_value('check_date', date)
+		}else if (frm.doc.time_unit === 'Next Monday') {
+			let dayOfWeek = 1;
+			let date = new Date(get_today());
+			date.setDate(date.getDate() + (7 + dayOfWeek - date.getDay() - 1) % 7 +1);
+			frm.set_value('check_date', date)
+		}else if (frm.doc.time_unit === 'Next Tuesday') {
+			let dayOfWeek = 2;
+			let date = new Date(get_today());
+			date.setDate(date.getDate() + (7 + dayOfWeek - date.getDay() - 1) % 7 +1);
+			frm.set_value('check_date', date)
+		}else if (frm.doc.time_unit === 'Next Wednesday') {
+			let dayOfWeek = 3;
+			let date = new Date(get_today());
+			date.setDate(date.getDate() + (7 + dayOfWeek - date.getDay() - 1) % 7 +1);
+			frm.set_value('check_date', date)
+		}else if (frm.doc.time_unit === 'Next Thursday') {
+			let dayOfWeek = 4;
+			let date = new Date(get_today());
+			date.setDate(date.getDate() + (7 + dayOfWeek - date.getDay() - 1) % 7 +1);
+			frm.set_value('check_date', date)
+		}else if (frm.doc.time_unit === 'Next Friday') {
+			let dayOfWeek = 5;
+			let date = new Date(get_today());
+			date.setDate(date.getDate() + (7 + dayOfWeek - date.getDay() - 1) % 7 +1);
+			frm.set_value('check_date', date)
+		}else if (frm.doc.time_unit === 'Next Saturday') {
+			let dayOfWeek = 6;
+			let date = new Date(get_today());
+			date.setDate(date.getDate() + (7 + dayOfWeek - date.getDay() - 1) % 7 +1);
+			frm.set_value('check_date', date)
+		}
+	},
+
+	refresh: function(frm) {
+		frm.toggle_display("process_payroll_section", frm.doc.status !== 'Initialize');
+		frm.toggle_display("qc_payroll_section", frm.doc.status !== 'Initialize' && frm.doc.status !== 'Process');
+		frm.toggle_display("dispatch_payroll_section", frm.doc.status !== 'Initialize' && frm.doc.status !== 'Process' && frm.doc.status !== 'QC');
+		frm.set_df_property("select_payroll_type", "read_only", frm.doc.status !== 'Initialize');
+		frm.set_df_property("store_name", "read_only", frm.doc.status !== 'Initialize');
+		frm.set_df_property("date", "read_only", frm.doc.status !== 'Initialize');
+		frm.set_df_property("payroll_schedule", "read_only", frm.doc.status !== 'Initialize');
+		frm.set_df_property("time_unit", "read_only", frm.doc.status !== 'Initialize');
+		frm.set_df_property("pay_start_date", "read_only", frm.doc.status !== 'Initialize');
+		frm.set_df_property("pay_end_date", "read_only", frm.doc.status !== 'Initialize');
+		frm.set_df_property("check_date", "read_only", frm.doc.status !== 'Initialize');
+
+		frm.set_df_property("direct_deposit", "read_only", frm.doc.status !== 'Process');
+
+		
+
+		frm.set_df_property("No. of Pay Checks", "read_only", frm.doc.status !== 'Process');
+		frm.set_df_property("pay_stubs", "read_only", frm.doc.status !== 'Process');
+		frm.set_df_property("checks", "read_only", frm.doc.status !== 'Process');
+		frm.set_df_property("remarks", "read_only", frm.doc.status !== 'Process');
+
+
+		frm.set_df_property("abc_check", "read_only", frm.doc.status !== 'QC');
+
+
+		frm.set_df_property('print_report', 'reqd', (frm.doc.status === 'Dispatch') ? 1 : 0);
+		frm.set_df_property('tracking_no', 'reqd', (frm.doc.status === 'Dispatch') ? 1 : 0);
+		frm.set_df_property('mode_of_delivery', 'reqd', (frm.doc.status === 'Dispatch') ? 1 : 0);
+	
+		
+		// frm.set_df_property("direct_deposit", "read_only", frm.doc.status === 0);
+
 	},
 });
--- payroll/payroll/doctype/payrolls/payrolls.py
+++ payroll/payroll/doctype/payrolls/payrolls.py
@@ -4,12 +4,22 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import throw, _
 from frappe.model.document import Document
 
 class Payrolls(Document):
 	def validate(self):
+		self.checks_validations()
+		self.start_end_dates_validations()
+
+	def checks_validations(self):
 		if self.no_of_direct_deposit or self.no_of_pay_checks:
 			self.total_no_of_pay_checks = self.no_of_direct_deposit + self.no_of_pay_checks
 		else:
 			self.total_no_of_pay_checks = 0
+
+
+	def start_end_dates_validations(self):
+		if self.pay_start_date > self.pay_end_date:
+			frappe.throw(_('Pay Start Date must be less than Pay End Date!'))
 
